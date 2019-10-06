7f5bbc788c77f8d0d0ff004f5bfbc8fdcc46146d
Oriol Nieto <oriol.nieto@gmail.com>
2019-10-06 15:51:48 -0700

    Sleeping for an hour before doing another request

--- src/get_youtube_urls.py
+++ src/get_youtube_urls.py
@@ -13,7 +13,7 @@ from urllib.error import HTTPError
 
 METADATA_CSV = "../dataset/metadata.csv"
 OUT_CSV = "youtube_urls.csv"
-SLEEP = 30
+SLEEP = 3600
 
 
 def process():
@@ -24,14 +24,14 @@ def process():
     urls = []
     for i, row in tqdm(df.iterrows(), total=len(df)):
         query = row["Title"] + ' ' + row["Artist"]
+        query_string = urllib.parse.urlencode({"search_query": query})
         try:
-            query_string = urllib.parse.urlencode({"search_query": query})
+            html_content = urllib.request.urlopen(
+                "http://www.youtube.com/results?" + query_string)
         except HTTPError:
             time.sleep(SLEEP)
-            query_string = urllib.parse.urlencode({"search_query": query})
-
-        html_content = urllib.request.urlopen(
-            "http://www.youtube.com/results?" + query_string)
+            html_content = urllib.request.urlopen(
+                "http://www.youtube.com/results?" + query_string)
         search_results = re.findall(
             r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
         urls.append({
